b0fd80d5811eac30b993cfecb1d13bc8876a3d6d
ali rafiei <103249712+alirafiei75@users.noreply.github.com>
2022-05-05 21:51:19 +0430

    create render html file

--- website/views.py
+++ website/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index_view(request):
-    return HttpResponse('<h1>Home Page</h1>')
+    return render(request, 'index.html')
 
 def about_view(request):
     return HttpResponse('<h1>About Me</h1>')
