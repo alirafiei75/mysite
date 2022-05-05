31b075cec8c1d72d551d9962e47ead54c5e0f01a
ali rafiei <103249712+alirafiei75@users.noreply.github.com>
2022-05-05 21:09:31 +0430

    create home, contact and about pages

--- website/views.py
+++ website/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index_view(request):
+    return HttpResponse('<h1>Home Page</h1>')
+
+def about_view(request):
+    return HttpResponse('<h1>About Me</h1>')
+
+def contact_view(request):
+    return HttpResponse('<h1>Contact Me</h1>')
+
