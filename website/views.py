c28bba276d4e2ae84f08608f5af8c24cdd796f58
ali rafiei <103249712+alirafiei75@users.noreply.github.com>
2022-05-13 20:10:52 +0430

    added static urls and directories
    static and media url and root

--- website/views.py
+++ website/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index_view(request):
-    return render(request, 'index.html')
+    return render(request, 'website/index.html')
 
 def about_view(request):
     return HttpResponse('<h1>About Me</h1>')
